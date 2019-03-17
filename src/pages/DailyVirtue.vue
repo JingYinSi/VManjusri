40afded7a3bb23fdc1671c4338713890cb3ae70a
clx1608@qq.com <hbdennischen@outlook.com>
2019-03-17 15:18:56 +0800

    page DailyVirtue

---
+++ src/pages/DailyVirtue.vue
@@ -0,0 +1,28 @@
+<template>
+  <contribution-list v-bind="props" @selected="onSelected"/>
+</template>
+
+<script>
+import lampItems from '../helpers/LampItems.js'
+import ContributionList from '../components/ContributionList'
+export default {
+  components: {
+    ContributionList
+  },
+  computed: {
+    props () {
+      const items = {
+        ...lampItems,
+        afterNum: '人已供灯',
+        titleStyle: 'width:75px'
+      }
+      return items
+    }
+  },
+  methods: {
+    onSelected (index) {
+      this.$router.push({name: 'forprice', params: {type: lampItems.type, id: index}})
+    }
+  }
+}
+</script>
