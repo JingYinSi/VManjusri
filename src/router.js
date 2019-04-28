bad14a7558794f213b97d764a080df6fc4cdf8ee
clx1608@qq.com <hbdennischen@outlook.com>
2019-04-28 17:59:28 +0800

    test wx login

--- src/router.js
+++ src/router.js
@@ -9,11 +9,11 @@ function redirectToWechatAuth2 (redirectUrl) {
   const appId = 'wx6fd4695fd38a8b3f'
   const oauth2BaseURL = 'https://open.weixin.qq.com/connect/oauth2/authorize'
   const wrapedUrl = `${oauth2BaseURL}?appid=${appId}&redirect_uri=${redirectUrl}&response_type=code&scope=snsapi_userinfo#wechat_redirect`
-
+  window.alter(wrapedUrl)
   window.location.href = encodeURI(wrapedUrl)
 }
 
-async function __beforeEach (to, from, next) {
+/* async function __beforeEach (to, from, next) {
   if (to.name === 'home') {
     router.replace({
       name: 'lamps'
@@ -30,6 +30,30 @@ async function __beforeEach (to, from, next) {
       // redirectToWechatAuth2(redirectUrl)
       redirectToWechatAuth2('http://dev.jingyintemple.top/jingyin/rests/manjusri/wx/signin')
       window.alert('aaaaaaaaaaaaaaaa')
+      return router.replace(to)
+    }
+    window.alert('code: ' + code)
+    await store.dispatch('wechatSignin', code) // 获取用户信息,后端可首先通过cookie,session等判断,没有信息则通过code获取
+  }
+  next()
+} */
+
+async function __beforeEach (to, from, next) {
+  if (to.name === 'home') {
+    router.replace({
+      name: 'lamps'
+    })
+  } else if (!to.matched.some(item => item.meta.noAuth) && !store.getters.user) {
+    const code = new URL(window.location.href).searchParams.get('code') // 截取url上的code ,可能没有,则返回''空字符串
+    if (!code || code.length === 0) {
+      // 跳转到微信授权页面
+      window.alter(window.location.origin)
+      window.alter(to.fullPath)
+      // 这个redirectUrl用 当前页路径或者tof.fullPath(将要进入的路径)
+      let redirectUrl = window.location.href
+      window.alter(redirectUrl)
+      redirectToWechatAuth2(redirectUrl)
+      // redirectToWechatAuth2('http://dev.jingyintemple.top/jingyin/rests/manjusri/wx/signin')
       return router.replace(to)
     }
     window.alert('code: ' + code)
