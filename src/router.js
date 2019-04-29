a9e62dad8f78a82ae06c3abb3f847549a977d8b3
clx1608@qq.com <hbdennischen@outlook.com>
2019-04-29 16:49:50 +0800

    test wx login

--- src/router.js
+++ src/router.js
@@ -6,12 +6,11 @@ import store from './store'
 Vue.use(VueRouter)
 
 function redirectToWechatAuth2 (redirectUrl) {
-  alert(redirectUrl)
   const appId = 'wx6fd4695fd38a8b3f'
   const oauth2BaseURL = 'https://open.weixin.qq.com/connect/oauth2/authorize'
   const wrapedUrl = `${oauth2BaseURL}?appid=${appId}&redirect_uri=${redirectUrl}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
   // const wrapedUrl = `${oauth2BaseURL}?appid=${appId}&redirect_uri=${redirectUrl}&response_type=code&scope=snsapi_base&state=123#wechat_redirect`
-  alert(wrapedUrl)
+  alert('redirect to ' + wrapedUrl)
   window.location.href = wrapedUrl
   // window.location.href = encodeURI(wrapedUrl)
 }
@@ -42,12 +41,12 @@ function redirectToWechatAuth2 (redirectUrl) {
 } */
 
 async function __beforeEach (to, from, next) {
+  alert('enter beforeEach ......')
   if (to.name === 'home') {
     router.replace({
       name: 'lamps'
     })
   } else if (!to.matched.some(item => item.meta.noAuth) && !store.getters.user) {
-    // alert('aaaaaaaaaaaa')
     const code = new URL(window.location.href).searchParams.get('code') // 截取url上的code ,可能没有,则返回''空字符串
     if (!code || code.length === 0) {
       // 跳转到微信授权页面
@@ -58,7 +57,7 @@ async function __beforeEach (to, from, next) {
       alert(`redirectUrl is:${redirectUrl}`)
       redirectToWechatAuth2(redirectUrl)
       // redirectToWechatAuth2('http://dev.jingyintemple.top/jingyin/rests/manjusri/wx/signin')
-      return
+      return false
     }
     alert('code: ' + code)
     await store.dispatch('wechatSignin', code) // 获取用户信息,后端可首先通过cookie,session等判断,没有信息则通过code获取
