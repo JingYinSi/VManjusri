4a01e374e06dc02d1122206125f52fda848b73c8
clx1608@qq.com <hbdennischen@outlook.com>
2019-04-29 17:17:03 +0800

    test wx login

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import store from './store'
 
 Vue.use(VueRouter)
 
-function redirectToWechatAuth2 (redirectUrl) {
+/* function redirectToWechatAuth2(redirectUrl) {
   const appId = 'wx6fd4695fd38a8b3f'
   const oauth2BaseURL = 'https://open.weixin.qq.com/connect/oauth2/authorize'
   const wrapedUrl = `${oauth2BaseURL}?appid=${appId}&redirect_uri=${redirectUrl}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
@@ -13,7 +13,7 @@ function redirectToWechatAuth2 (redirectUrl) {
   alert('redirect to ' + wrapedUrl)
   window.location.href = wrapedUrl
   // window.location.href = encodeURI(wrapedUrl)
-}
+} */
 
 /* async function __beforeEach (to, from, next) {
   if (to.name === 'home') {
@@ -47,7 +47,8 @@ async function __beforeEach (to, from, next) {
       name: 'lamps'
     })
   } else if (to.matched.some(item => !item.meta.noAuth) && !store.getters.user) {
-    const code = new URL(window.location.href).searchParams.get('code') // 截取url上的code ,可能没有,则返回''空字符串
+    alert('we are going to auth')
+    /* const code = new URL(window.location.href).searchParams.get('code') // 截取url上的code ,可能没有,则返回''空字符串
     if (!code || code.length === 0) {
       // 跳转到微信授权页面
       alert('orignurl is: ' + window.location.origin)
@@ -57,17 +58,16 @@ async function __beforeEach (to, from, next) {
       alert(`redirectUrl is:${redirectUrl}`)
       redirectToWechatAuth2(redirectUrl)
       // redirectToWechatAuth2('http://dev.jingyintemple.top/jingyin/rests/manjusri/wx/signin')
-      return false
-    }
-    alert('code: ' + code)
-    await store.dispatch('wechatSignin', code) // 获取用户信息,后端可首先通过cookie,session等判断,没有信息则通过code获取
+      return false */
   }
   next()
+  /* alert('code: ' + code)
+  await store.dispatch('wechatSignin', code) // 获取用户信息,后端可首先通过cookie,session等判断,没有信息则通过code获取 */
 }
 
 const router = new VueRouter({
-  routes,
-  mode: 'history'
+  routes // ,
+  // mode: 'history'
 })
 router.beforeEach(__beforeEach)
 
