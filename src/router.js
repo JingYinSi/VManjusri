c8c97fecc2c6b51d0d2490092ce8660926d7e898
clx1608@qq.com <hbdennischen@outlook.com>
2019-04-29 11:30:54 +0800

    test wx login

--- src/router.js
+++ src/router.js
@@ -54,7 +54,7 @@ async function __beforeEach (to, from, next) {
       alert('orignurl is: ' + window.location.origin)
       alert('location.href is: ' + window.location.href)
       // 这个redirectUrl用 当前页路径或者tof.fullPath(将要进入的路径)
-      let redirectUrl = window.location.origin + '/index.html?from=singlemessage#' + to.fullPath
+      let redirectUrl = window.location.origin + '/index.html#' + to.fullPath
       alert(`redirectUrl is:${redirectUrl}`)
       redirectToWechatAuth2(redirectUrl)
       // redirectToWechatAuth2('http://dev.jingyintemple.top/jingyin/rests/manjusri/wx/signin')
